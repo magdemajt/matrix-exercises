dd23b926638e1436d65794333fafdba71139b7da
Mateusz Wejman <mateusz.piotr.wejman@gmail.com>
2024-03-13 20:38:33 +0100

    Strassen

--- test/lib/test.ex
+++ test/lib/test.ex
@@ -10,6 +10,9 @@ defmodule Test do
           Enum.reduce(0..size-1, 0, fn k, acc ->a[i][k] * b[k][j] + acc end)))
   end
 
+# make multi but with enum
+
+
   def strassen(a, b, exp, l) do
     a11 = Arrays.new(for i <- 0..Math.pow(2, l-1)-1, do:
             Arrays.new(for j <- 0..Math.pow(2, l-1)-1, do: a[i][j]))
