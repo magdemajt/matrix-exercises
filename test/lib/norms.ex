07cb9f464818ec6a3f8b5843bcb3b3a084123bef
Mateusz Wejman <mateusz.piotr.wejman@gmail.com>
2024-04-15 19:44:07 +0200

    Added norm p and inf and 2

--- test/lib/norms.ex
+++ test/lib/norms.ex
@@ -1,12 +1,75 @@
-defmodule MatrixNorm do
-  @size = 3
+defmodule Norms do
+  @moduledoc false
 
-  def norm1(matrix) do
-    cols_sum = for _ <- 0..size-1, do: 0
-    res = Enum.reduce(0..size-1, cols_sum, fn row, acc ->
-      for cell <- 0..size-1, do: Enum.at(cols_sum, cell) = Enum.at(matrix, row) |> Enum.at(cell)
+  alias Matrix
+  alias MatrixOperation
+
+  @iter 1000
+
+  @matrix_size 3
+
+  def norm_m_1(matrix) do
+    {rows, cols} = Matrix.size(matrix)
+
+#   sum each column and take max
+    Enum.reduce(0..(cols - 1), 0, fn j, acc ->
+      Kernel.max(acc, Enum.reduce(0..(rows - 1), 0, fn i, acc ->
+        acc + Matrix.elem(matrix, i, j)
+      end))
     end)
-    |> Enum.max(cols_sum)
-    res
+  end
+
+  def norm_m_inf(matrix) do
+    {rows, cols} = Matrix.size(matrix)
+
+    Enum.reduce(0..(rows - 1), 0, fn i, acc ->
+      Kernel.max(acc, Enum.reduce(0..(cols - 1), 0, fn j, acc ->
+        acc + Matrix.elem(matrix, i, j)
+      end))
+    end)
+  end
+
+  def norm_m_2(matrix) do
+    {rows, cols} = Matrix.size(matrix)
+
+    transposed = Matrix.transpose(matrix)
+
+    multiplied = Matrix.multiply(transposed, matrix)
+
+    multiplied |>
+    MatrixOperation.eigen()
+    |> Enum.reduce(0, fn {_, value}, acc -> Kernel.max(acc, value) end)
+    |> :math.sqrt()
+  end
+
+  def norm_m_p(matrix, p) do
+    case p do
+      1 -> norm_m_1(matrix)
+      2 -> norm_m_2(matrix)
+      :inf -> norm_m_inf(matrix)
+      _ -> points = Enum.map(0..(@matrix_size - 2), fn _ -> :rand.uniform()  end)
+           last_point = 1 - Enum.reduce(points, 0, fn x, acc -> acc + :math.pow(x, p) end) |> :math.pow(1/p)
+           points = points ++ [last_point]
+           maximal = Enum.reduce(0..@iter, 0, fn _, acc -> Kernel.max(
+                                                             acc,
+                                                             Matrix.mult(matrix, points)
+                                                             |> Enum.map(fn x -> :math.pow(x, p) end)
+                                                             |> Enum.reduce(0, fn x, acc -> acc + x end)
+                                                             |> :math.pow(1/p))
+           end)
+           maximal
+    end
+  end
+
+  def cond_m_1(matrix) do
+    norm_m_1(matrix) * norm_m_1(Matrix.inv(matrix))
+  end
+
+  def cond_m_2(matrix) do
+    norm_m_2(matrix) * norm_m_2(Matrix.inv(matrix))
+  end
+
+  def cond_m_p(matrix, p) do
+    norm_m_p(matrix, p) * norm_m_p(Matrix.inv(matrix), p)
   end
 end
